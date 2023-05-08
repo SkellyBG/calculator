718432b78751f4c16f125445a685cb21379c556f
SkellyBG <skellybg245@gmail.com>
2023-05-08 23:16:19 +1000

    populate the display when adding number

--- script.js
+++ script.js
@@ -14,9 +14,28 @@ const divide = function divide(num1, num2) {
     return num1 / num2;
 };
 
-let num1;
-let operator;
-let num2;
+let num1 = 0;
+let operator = '+';
+let num2 = 0;
+
+
+document.querySelector(".num-1").addEventListener("click", function(){populateDisplay(1);})
+document.querySelector(".num-2").addEventListener("click", function(){populateDisplay(2);})
+document.querySelector(".num-3").addEventListener("click", function(){populateDisplay(3);})
+document.querySelector(".num-4").addEventListener("click", function(){populateDisplay(4);})
+document.querySelector(".num-5").addEventListener("click", function(){populateDisplay(5);})
+document.querySelector(".num-6").addEventListener("click", function(){populateDisplay(6);})
+document.querySelector(".num-7").addEventListener("click", function(){populateDisplay(7);})
+document.querySelector(".num-8").addEventListener("click", function(){populateDisplay(8);})
+document.querySelector(".num-9").addEventListener("click", function(){populateDisplay(9);})
+document.querySelector(".num-0").addEventListener("click", function(){populateDisplay(0);})
+
+const populateDisplay = function populateDisplay(digit) {
+    const display = document.querySelector(".display");
+    let num = Number.parseInt(display.textContent);
+    num = num * 10 + digit;
+    display.textContent = num.toString();
+};
 
 const operate = function(num1, num2, operator) {
     switch(operator) {
